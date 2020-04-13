4dbb9eb1e8ec4fad993d85687d924fb2172e685a
Poonnada-C <s6101012610126@email.kmutnb.ac.th>
2020-04-13 15:36:14 +0700

    Add Some Comment to views.py

--- notes/views.py
+++ notes/views.py
@@ -7,51 +7,52 @@ from django.http import HttpResponseNotFound
 from .models import Note, Image, Tag, Review
 from django.template.loader import get_template
 # Create your views here.
-def ClearStrSpace(input):
+def ClearStrSpace(input):  #delete excess space
     return " ".join(input.split())
 
 def home_page(request):
-    notes = Note.objects.order_by('-upload_time')
-    return render(request,'home_page.html',{'notes':notes})
+    notes = Note.objects.order_by('-upload_time')  #get notes from db and order them by upload time
+    return render(request,'home_page.html',{'notes':notes})  #return from home_page.html with notes
 def upload_page(request):
-    return render(request,'upload_page.html')
+    return render(request,'upload_page.html')  #return from upload_page.html
 def upload_api(request):
     filetype_list = ["img", "png", "jpg" ,"jpeg", "tiff", "gif", "bmp"]
     if request.POST:
         newnote = Note()
-        newnote.name  =  ClearStrSpace(request.POST['name'])
-        newnote.owner =  ClearStrSpace(request.POST['guestname'])
-        newnote.desc  =  ClearStrSpace(request.POST['desc'])
-        newnote.save()
+        newnote.name  =  ClearStrSpace(request.POST['name'])  #delete excess space and set note name
+        newnote.owner =  ClearStrSpace(request.POST['guestname'])  #delete excess space and set owner name
+        newnote.desc  =  ClearStrSpace(request.POST['desc'])  #delete excess space and set description
+        newnote.save()  #save note to database
 
         i = 0
+        
         for file in request.FILES.getlist('myfile'):
             if(len(file.name.split(".")) > 1 and
-                    file.name.split(".")[-1].lower() in filetype_list):
+                    file.name.split(".")[-1].lower() in filetype_list):    #check file type (image type)
                 newimg = Image()
-                newimg.image = file
-                newimg.index = i
-                newimg.note = newnote
-                newimg.save()
+                newimg.image = file   #set image to current file
+                newimg.index = i   #set image index
+                newimg.note = newnote   #set note to newnote
+                newimg.save()   #save image to database
                 i += 1
         if(i==0): #all file is invalid
-            newnote.delete()
-            return HttpResponse("File Type Error")
+            newnote.delete()    #delete note
+            return HttpResponse("File Type Error")  #return file type error
         
-        return HttpResponseRedirect('/')
-    return HttpResponseRedirect('/')
+        return HttpResponseRedirect('/')   #return to homepage
+    return HttpResponseRedirect('/')   #return to homepage
 
 def detial(request,note_index):
     n = get_object_or_404(Note, pk=note_index)
     images = Image.objects.filter(note=n)
     img_url = [i.image.url for i in images]
-    return render(request,'detail.html',{'images_url':img_url,'note':n})
+    return render(request,'detail.html',{'images_url':img_url,'note':n})  #return from detail.html with image_urls and notes
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, 'about.html')  #return from about_page.html
 
 def help(request):
-    return render(request, 'help_main.html')
+    return render(request, 'help_main.html')  #return from help_main.html
 
 def help_detail(request, help_topic):
     try:
